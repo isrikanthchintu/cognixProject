4c58767850e2e40ae90ad6fdb006e2e4c28e91cd
Srikanth <shiva.kotha@cognixsoft.com>
2025-10-16 18:45:40 +0530

    fest

--- src/test/java/com/example/CogniXProject/CogniXProjectApplicationTests.java
+++ src/test/java/com/example/CogniXProject/CogniXProjectApplicationTests.java
@@ -8,6 +8,15 @@ class CogniXProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("The first test case ran successfully");
 	}
 
+	@Test
+	void testCase1() {
+		System.out.println("The first test case ran successfully");
+	}
+	@Test
+	void testCase12() {
+		System.out.println("The second test case ran successfully");
+	}
 }
