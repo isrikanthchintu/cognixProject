386ef4f636ff941b57cf3d46e4184c790a98ac6d
Srikanth <shiva.kotha@cognixsoft.com>
2025-10-16 18:25:41 +0530

    initisl commit

---
+++ src/main/java/com/example/CogniXProject/CogniXProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.CogniXProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogniXProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("the project in jenkins");
+		SpringApplication.run(CogniXProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CogniXProject/CogniXProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CogniXProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogniXProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
