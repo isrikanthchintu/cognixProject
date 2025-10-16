1306103305b80641daf82205ef568e57a596034b
Srikanth <shiva.kotha@cognixsoft.com>
2025-10-16 18:34:58 +0530

    fest

--- src/main/java/com/example/CogniXProject/CogniXProjectApplication.java
+++ src/main/java/com/example/CogniXProject/CogniXProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CogniXProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("the project in jenkins  ");
+		System.out.println("the projec t in jenkins  ");
 		SpringApplication.run(CogniXProjectApplication.class, args);
 	}
 
