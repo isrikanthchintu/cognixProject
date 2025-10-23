d33a191b6d287dd41e7a0a2f672d59f48e4cfe8a
Srikanth Porandla <139649941+isrikanthchintu@users.noreply.github.com>
2025-10-23 17:13:27 +0530

    Update CogniXProjectApplication.java
    lo

--- src/main/java/com/example/CogniXProject/CogniXProjectApplication.java
+++ src/main/java/com/example/CogniXProject/CogniXProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CogniXProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("this project in Jenkinsfile ");
+		System.out.println("this project in Jenkinsfile    ");
 		SpringApplication.run(CogniXProjectApplication.class, args);
 	}
 
